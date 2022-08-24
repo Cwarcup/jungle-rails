05019e9f0f982de2bdb9a186aeed1ff4e04a68cf
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-08-24 12:31:31 -0700

    Display count for admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,16 @@
 class Admin::DashboardController < ApplicationController
   include HttpAuthConcern
 
-  def show; end
+  #  have the controller fetch data from database using active record
+
+  # set instance variable in controller
+  #  then render the view with the data "/views/admin/dashboard/show.html.erb"
+
+  #  desired data
+  # Display a count of how many products are in the database
+  # Display a count of how many categories are in the database
+  def show
+    @products = Product.count
+    @categories = Category.count
+  end
 end
