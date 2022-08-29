a4d91771f90735d3cad6626c2e37efb6e1c595f4
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-08-29 16:11:07 -0700

    Basic cypress homepage tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -10,4 +10,7 @@ describe("Homepage", () => {
 	it("There is products on the page", () => {
 		cy.get(".products article").should("be.visible");
 	});
+	it("There is 2 products on the page", () => {
+		cy.get(".products article").should("have.length", 2);
+	});
 });
