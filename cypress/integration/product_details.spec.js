e3d45762683d500a8ee26e9734a9370463b79e47
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-08-29 16:26:28 -0700

    Cypress for product details

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,17 @@
+describe("Homepage", () => {
+	beforeEach(() => {
+		cy.visit("/");
+	});
+	it("should go to the homepage", () => {
+		cy.visit("/");
+		//  title should include "Jungle"
+		cy.title().should("include", "Jungle");
+	});
+
+	it("should go to the product details page of the first item", () => {
+		cy.get(".products article").first().click();
+		//  title should include "Jungle"
+		cy.get(".product-detail").should("be.visible");
+		cy.get(".product-detail h1").should("have.text", "Scented Blade");
+	});
+});
